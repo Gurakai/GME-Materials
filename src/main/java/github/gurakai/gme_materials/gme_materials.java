7d6b3410f2ab9e166761f764d863a972ad2da5fd
Gurakai <guildzeda@gmail.com>
2024-08-24 15:05:47 -0500

    first commit

---
+++ src/main/java/github/gurakai/gme_materials/gme_materials.java
@@ -0,0 +1,17 @@
+package github.gurakai.gme_materials;
+
+import github.gurakai.gme_materials.initilization.ItemInitialization;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
+@Mod(gme_materials.MODID)
+public class gme_materials {
+    public static final String MODID = "gme_materials";
+
+    public gme_materials() {
+        IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        ItemInitialization.ITEMS.register(bus);
+    }
+}
---
+++ src/main/java/github/gurakai/gme_materials/initilization/ItemInitialization.java
@@ -0,0 +1,26 @@
+package github.gurakai.gme_materials.initilization;
+
+import github.gurakai.gme_materials.gme_materials;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.food.FoodProperties;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.Rarity;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ItemInitialization {
+    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, gme_materials.MODID);
+
+    public static final RegistryObject<Item> electrum_apple = ITEMS.register("electrum_apple",
+            () -> new Item(new Item.Properties()
+                    .stacksTo(64)
+                    .food(new FoodProperties.Builder()
+                            .nutrition(5)
+                            .saturationMod(0.2f)
+                            .effect(() -> new MobEffectInstance(MobEffects.ABSORPTION, 200, 2), 1f)
+                            .build())
+                    .rarity(Rarity.RARE)
+            ));
+}
