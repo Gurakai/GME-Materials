76c2d13bbc8444070d3a064276b207070abbb531
Gurakai <guildzeda@gmail.com>
2024-08-25 00:31:58 -0500

    added nuggets and guralloy block

--- src/main/java/github/gurakai/gme_materials/initilization/BlockInitialization.java
+++ src/main/java/github/gurakai/gme_materials/initilization/BlockInitialization.java
@@ -2,9 +2,11 @@ package github.gurakai.gme_materials.initilization;
 
 import github.gurakai.gme_materials.gme_materials;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.properties.NoteBlockInstrument;
 import net.minecraft.world.level.material.MapColor;
+import net.minecraft.world.level.material.PushReaction;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
@@ -14,9 +16,13 @@ import javax.swing.*;
 public class BlockInitialization {
     public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, gme_materials.MODID);
 
-    public static final RegistryObject<Block> block_electrum = BLOCKS.register("block_electrum",
+    public static final RegistryObject<Block> ingot_block_guralloy = BLOCKS.register("ingot_block_guralloy",
             () -> new Block(BlockBehaviour.Properties.of()
-                    .mapColor(MapColor.COLOR_GREEN)
+                    .mapColor(MapColor.COLOR_YELLOW)
                     .strength(5.0f, 17f)
-                    .instrument(NoteBlockInstrument.BELL)))
+                    .instrument(NoteBlockInstrument.BELL)
+                    .lightLevel(state -> 10)
+                    .requiresCorrectToolForDrops()
+                    .pushReaction(PushReaction.NORMAL)
+            ));
 }
--- src/main/java/github/gurakai/gme_materials/initilization/ItemInitialization.java
+++ src/main/java/github/gurakai/gme_materials/initilization/ItemInitialization.java
@@ -4,6 +4,7 @@ import github.gurakai.gme_materials.gme_materials;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.food.FoodProperties;
+import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.Rarity;
 import net.minecraftforge.registries.DeferredRegister;
@@ -13,20 +14,20 @@ import net.minecraftforge.registries.RegistryObject;
 public class ItemInitialization {
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, gme_materials.MODID);
 
-    public static final RegistryObject<Item> electrum_apple = ITEMS.register("electrum_apple",
+    public static final RegistryObject<Item> guralloy_apple = ITEMS.register("guralloy_apple",
             () -> new Item(new Item.Properties()
                     .stacksTo(64)
                     .food(new FoodProperties.Builder()
                             .nutrition(5)
                             .saturationMod(0.2f)
-                            .effect(() -> new MobEffectInstance(MobEffects.ABSORPTION, 200, 2), 1f)
+                            .effect(() -> new MobEffectInstance(MobEffects.ABSORPTION, 2400, 3), 1f)
                             .build())
                     .rarity(Rarity.RARE)
             ));
 
     //ingots
 
-    public static final RegistryObject<Item> ingot_electrum = ITEMS.register("ingot_electrum",
+    public static final RegistryObject<Item> ingot_guralloy = ITEMS.register("ingot_guralloy",
             () -> new Item(new Item.Properties()
                     .stacksTo(64)
                     .rarity(Rarity.RARE)
@@ -35,28 +36,65 @@ public class ItemInitialization {
     public static final RegistryObject<Item> ingot_invar = ITEMS.register("ingot_invar",
             () -> new Item(new Item.Properties()
                     .stacksTo(64)
-                    .rarity(Rarity.RARE)
+                    .rarity(Rarity.UNCOMMON)
             ));
 
     public static final RegistryObject<Item> ingot_nickel = ITEMS.register("ingot_nickel",
             () -> new Item(new Item.Properties()
                     .stacksTo(64)
-                    .rarity(Rarity.RARE)
+                    .rarity(Rarity.COMMON)
             ));
 
-    public static final RegistryObject<Item> ingot_red_silverium = ITEMS.register("ingot_red_silverium",
+    public static final RegistryObject<Item> ingot_red_silvarium = ITEMS.register("ingot_red_silvarium",
             () -> new Item(new Item.Properties()
                     .stacksTo(64)
-                    .rarity(Rarity.RARE)
+                    .rarity(Rarity.UNCOMMON)
             ));
 
     public static final RegistryObject<Item> ingot_silver = ITEMS.register("ingot_silver",
             () -> new Item(new Item.Properties()
                     .stacksTo(64)
-                    .rarity(Rarity.RARE)
+                    .rarity(Rarity.COMMON)
             ));
 
     //nuggets
 
+    public static final RegistryObject<Item> nugget_guralloy = ITEMS.register("nugget_guralloy",
+            () -> new Item(new Item.Properties()
+                    .stacksTo(64)
+                    .rarity(Rarity.RARE)
+            ));
 
+    public static final RegistryObject<Item> nugget_invar = ITEMS.register("nugget_invar",
+            () -> new Item(new Item.Properties()
+                    .stacksTo(64)
+                    .rarity(Rarity.UNCOMMON)
+            ));
+
+    public static final RegistryObject<Item> nugget_nickel = ITEMS.register("nugget_nickel",
+            () -> new Item(new Item.Properties()
+                    .stacksTo(64)
+                    .rarity(Rarity.COMMON)
+            ));
+
+    public static final RegistryObject<Item> nugget_red_silvarium = ITEMS.register("nugget_red_silvarium",
+            () -> new Item(new Item.Properties()
+                    .stacksTo(64)
+                    .rarity(Rarity.UNCOMMON)
+            ));
+
+    public static final RegistryObject<Item> nugget_silver = ITEMS.register("nugget_silver",
+            () -> new Item(new Item.Properties()
+                    .stacksTo(64)
+                    .rarity(Rarity.COMMON)
+            ));
+
+    //ingot blocks
+
+    public static final RegistryObject<Item> ingot_block_guralloy = ITEMS.register("ingot_block_guralloy",
+            () -> new BlockItem(BlockInitialization.ingot_block_guralloy.get(),
+                    new Item.Properties()
+                            .stacksTo(64)
+                            .rarity(Rarity.RARE)
+            ));
 }
